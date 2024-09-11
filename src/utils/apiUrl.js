74417b63f285135e83dee1c411b4fae8d964f499
geon <yy4338@gmail.com>
2024-09-11 09:46:48 +0900

    Docker

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,9 +1,9 @@
-const GET_TASKS_API_URL = 'http://localhost:8080/get_tasks';
-const POST_TASKS_API_URL = 'http://localhost:8080/post_task';
-const DELETE_TASKS_API_URL = 'http://localhost:8080/delete_task';
+const GET_TASKS_API_URL = 'https://taskbackend.geonnim.com/get_tasks';
+const POST_TASKS_API_URL = 'https://taskbackend.geonnim.com/post_task';
+const DELETE_TASKS_API_URL = 'https://taskbackend.geonnim.com/delete_task';
 const UPDATE_COMPLETED_TASKS_API_URL =
-  'http://localhost:8080/update_completed_task';
-const UPDATE_TASK_API_URL = 'http://localhost:8080/update_task';
+  'https://taskbackend.geonnim.com/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://taskbackend.geonnim.com/update_task';
 
 export {
   GET_TASKS_API_URL,
