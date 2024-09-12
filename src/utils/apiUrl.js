953e6b8de2616d44b193b5e6853a4c1ed30ebe40
geon <yy4338@gmail.com>
2024-09-12 12:48:43 +0900

    url 숨김

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,9 +1,9 @@
-const GET_TASKS_API_URL = 'https://taskbackend.geonnim.com/get_tasks';
-const POST_TASKS_API_URL = 'https://taskbackend.geonnim.com/post_task';
-const DELETE_TASKS_API_URL = 'https://taskbackend.geonnim.com/delete_task';
+const GET_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_tasks`;
+const POST_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/post_task`;
+const DELETE_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/delete_task`;
 const UPDATE_COMPLETED_TASKS_API_URL =
-  'https://taskbackend.geonnim.com/update_completed_task';
-const UPDATE_TASK_API_URL = 'https://taskbackend.geonnim.com/update_task';
+  `${process.env.REACT_APP_MY_DOMAIN}/update_completed_task`;
+const UPDATE_TASK_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_task`;
 
 export {
   GET_TASKS_API_URL,
